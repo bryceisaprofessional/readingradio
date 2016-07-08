df221a379d08671d5d491d065dfa5ef8560afe1b
Law <U586268@DOW.COM>
2016-07-08 11:06:02 -0600

    Added comments

--- transcript_tool.py
+++ transcript_tool.py
@@ -1,10 +1,14 @@
+#Transcript tool to pull book references out of This American Life broadcasts.
+
 import urllib
 from HTMLParser import HTMLParser
 
+#Open and listify source
 def extract_source(url):
 	url_source = urllib.urlopen(url).read().decode('utf-8').encode('ascii', 'ignore')
 	return url_source
 
+#HTMLParser to grab data and strip tags
 class DataParser(HTMLParser):
 
 	def __init__(self):
@@ -14,6 +18,7 @@ class DataParser(HTMLParser):
 	def handle_data(self, data):
 		self.data_list.append(data)
 
+#specific search for books, author
 def book_search(array):
 	context_range = 30
 	index_count =  0
@@ -39,6 +44,7 @@ def book_search(array):
 
 	return context_array
 
+#Setup, may be looped to extract multiple references from multiple links
 
 thisamericanlife_source = extract_source\
 ("http://www.thisamericanlife.org/radio-archives/episode/445/transcript")

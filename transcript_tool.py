41db67505f6cc15106dee4eb08c08426d1547f6b
Law <U586268@DOW.COM>
2016-07-08 11:07:30 -0600

    additional comments

--- transcript_tool.py
+++ transcript_tool.py
@@ -3,7 +3,7 @@
 import urllib
 from HTMLParser import HTMLParser
 
-#Open and listify source
+#Open source as string
 def extract_source(url):
 	url_source = urllib.urlopen(url).read().decode('utf-8').encode('ascii', 'ignore')
 	return url_source
